900ea404f104fc1d49635719c0431725507695a4
witchicken <witchicken2@gmail.com>
2022-09-11 18:28:53 +0900

    ing-signupcss

--- login.php
+++ login.php
@@ -7,16 +7,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
+    <link rel="stylesheet" href="./src/css/reset.css" type="text/css"/>
+    <link rel="stylesheet" href="./src/css/Header.css" type="text/css" />
+    <link rel="stylesheet" href="./src/css/Footer.css" type="text/css" />
+    <link rel="stylesheet" href="./src/css/Login.css" type="text/css" />
+    <!--fontawesome!-->
+    <script src="https://kit.fontawesome.com/b4823e771e.js" crossorigin="anonymous"></script>
   </head>
   <body>
+<?php include_once 'header.inc' ?>
+
     <form class="login" action=<?php echo htmlspecialchars('login_ok.php');?>
     method="post">
     <fieldset>
-      <legend class="legend">Login</legend>
+      <h1>로그인</h1>
       <div class="input">
+      <i class="fa-solid fa-user"></i>
         <input type="email" placeholder="Email" name="email" required />
       </div>
       <div class="input">
+      <i class="fa-solid fa-lock"></i>
         <input
           type="password"
           placeholder="Password"
@@ -24,12 +34,18 @@
           required
         />
       </div>
-      <button type="submit" class="submit">로그인</button>
+      <div class="login_box">
+        <button type="submit" class="submit">로그인</button>
 
-      <a href="/signup.php" style="text-decoration: none; color: black">
-        회원가입
-      </a>
+        <a href="/signup.php" style="text-decoration: none; color: black">
+          회원가입
+        </a>
+      </div>
+      
     </fieldset>
     <div class="feedback"></div>
+    </form>
+    <?php include_once 'footer.inc' ?>
+  <script src="./src/js/Header.js" ></script>
   </body>
 </html>
--- thumbup.php
+++ thumbup.php
@@ -2,23 +2,55 @@
 
 <?php 
   $id = $_GET['id'];
-  $email = $_SESSION["user_email"];
-
-
-  include("./connectMysql.php");
-  $conn = new mysqli($servername,$username,$password,"enigspage");
-
-  $sql1 = "SELECT isthumbup FROM USER WHERE email = '$email'";
-
-  $prev_thumbup = mysqli_fetch_array(mysqli_query($conn,$sql1))['isthumbup'];
-
-  if($prev_thumbup == NULL){
-    $plusid = $id;
+  if(isset($_SESSION['user_email'])){
+    $email = $_SESSION["user_email"];
+    include("./connectMysql.php");
+    $conn = new mysqli($servername,$username,$password,"enigspage");
+  
+    $sql_select_isthumbup = "SELECT isthumbup FROM USER WHERE email = '$email'";
+  
+    $prev_thumbup = mysqli_fetch_array(mysqli_query($conn,$sql_select_isthumbup))['isthumbup'];
+  
+    if($prev_thumbup == NULL){
+      $plusid = $id;
+    }else{
+      $split_prev_thumbup = explode(',',$prev_thumbup);
+      $isexist_thumbup = false;
+      foreach ($split_prev_thumbup as $item) {
+        if($item === $id){
+          $isexist_thumbup = true;
+        }else{
+          
+        }
+      }
+      if($isexist_thumbup == true){
+        echo "<script>
+          alert('이미 추천 하였습니다.');
+          window.location.href='/read.php?id=$id';
+          </script>";
+      }else{
+        $plusid = $prev_thumbup . ',' . $id;
+        $sql_update_isthumbup = "UPDATE USER SET isthumbup = '$plusid' WHERE email = '$email'";
+        mysqli_query($conn,$sql_update_isthumbup);
+        $board = mysqli_fetch_array($conn->query("select thumbup from board where id = $id"));
+        $thumbup = $board['thumbup'] + 1;
+        mysqli_query($conn,"UPDATE BOARD SET thumbup = $thumbup where id = $id");    
+        echo "<script>
+          alert('추천하였습니다..');
+          window.location.href='/read.php?id=$id';
+          </script>";
+      }
+    }
+  $conn -> close();
+    
+    
   }else{
-    $plusid = $prev_thumbup . ',' . $id;
+    echo "<script>
+      alert('추천 권한이 없습니다.로그인 해 주세요.');
+      window.location.href='/read.php?id=$id';
+      </script>";
   }
 
-  $sql2 = "UPDATE USER SET isthumbup = '$plusid' WHERE email = '$email'";
-  mysqli_query($conn,$sql2);
-  $conn -> close();
+
+  
 ?>
